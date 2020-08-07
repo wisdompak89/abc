d9b6330e7154061993075ea7594fa83059ca4d8d
wisdompak89 <wisdompa@naver.com>
2020-08-07 11:43:12 +0900

    project(2)

--- crudapp/views.py
+++ crudapp/views.py
@@ -5,4 +5,14 @@ def home(requsest):
     return render(requsest, 'home.html')
 
 def new(request):
-    return render(request, 'new.html')
+    full_text = request.GET['fulltext']
+    word_list = full_text.split()
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    return render(request, 'new.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()} )
